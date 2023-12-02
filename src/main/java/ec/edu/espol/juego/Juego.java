9f1b28c4c2e7db8b900e64282ec949d37be7cccc
Allan Moisés Chacha León <142559140+allan23chacha@users.noreply.github.com>
2023-12-02 17:05:17 -0500

    commit hecho por allan chacha
    crear la clase juego con sus metodos

--- src/main/java/ec/edu/espol/juego/Juego.java
+++ src/main/java/ec/edu/espol/juego/Juego.java
@@ -3,11 +3,159 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ec.edu.espol.juego;
-
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
 /**
  *
- * @author user
+ * @author allan moises chacha leon 
  */
 public class Juego {
+     private ArrayList<Ficha> lineaJuego;
+    private ArrayList<Jugador> jugadores;
+    
+    Scanner sc = new Scanner(System.in);
+    
+    public JuegoPro()
+    {
+        lineaJuego = new ArrayList<>();
+        jugadores = new ArrayList<>();
+    }
+
+    public ArrayList<Ficha> getLineaJuego() {
+        return lineaJuego;
+    }
+
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+    
+    
+    
+    public void agregarJugador(String nombre)
+    {
+        Jugador jugador = new Jugador(nombre,Utilitaria.crearManoJugador());
+        jugadores.add(jugador);
+    }
+    
+    public int obtenerValorInicioLinea()
+    {
+        int lado1 = lineaJuego.get(0).getLado1();
+        return lado1;
+    }
+    
+    public int obtenerValorFinLinea()
+    {
+        int lado2 = lineaJuego.get(lineaJuego.size()-1).getLado2();
+        return lado2;
+    }
+    
+    public String mostrarLinea()
+    {
+        StringBuilder m = new StringBuilder();
+        int cont =0;
+        for(Ficha ficha: lineaJuego)
+        {
+            if(cont<(lineaJuego.size()-1))
+            {
+                m.append(ficha.toString()+"-");
+            }
+            else
+            {
+                m.append(ficha.toString());
+            }
+            cont++;
+        }
+        
+        return m.toString();
+    }
+    
+    public boolean agregarFichaLinea(Ficha ficha, Jugador jugador )
+    {
+
+        
+        boolean r =false;
+        if(!(ficha instanceof FichaComodin))
+        {
+            if(lineaJuego.size()==0)
+            {
+                lineaJuego.add(ficha);
+                jugador.removerFicha(ficha);
+                r= true;
+            }
+            else
+            {
+                if((ficha.getLado2()==this.obtenerValorInicioLinea()) || (ficha.getLado1()==this.obtenerValorFinLinea()))
+                {
+                    if(ficha.getLado2()==this.obtenerValorInicioLinea())
+                    {
+                        lineaJuego.add(0, ficha);
+                        jugador.removerFicha(ficha);
+                    }
+                    else
+                    {
+                        lineaJuego.add(ficha);
+                        jugador.removerFicha(ficha);
+                    }
+                    
+                    
+                    
+                    r= true;
+                    
+                }
+                else
+                {
+                    r= false;
+                }
+                
+                
+            }
+        }
+        else
+        {
+            FichaComodin comodin = (FichaComodin)ficha;
+            
+            if(lineaJuego.size()==0)
+            {
+                System.out.println("Movimiento valido");
+                lineaJuego.add(ficha);
+                System.out.println("Ingrese el lado1 de su ficha: ");
+                int lado1 = sc.nextInt();
+                System.out.println("Ingrese el lado2 de su ficha: ");
+                int lado2 = sc.nextInt();
+                comodin.setLado1(lado1);
+                comodin.setLado2(lado2);
+                jugador.removerFicha(ficha);
+                r = true;
+                
+            }
+            else
+            {
+                System.out.println("Movimiento valido");
+                System.out.println("Ingrese la posicion INICIO/FIN: ");
+                String pos = sc.next();
+                if(pos.toUpperCase().equals("INICIO"))
+                {
+                    lineaJuego.add(0, ficha);
+                    System.out.println("Ingrese el lado1 de su ficha: ");
+                    int lado1 = sc.nextInt();
+                    comodin.setLado1(lado1);
+                    jugador.removerFicha(ficha);
+                    
+                }
+                else
+                {
+                    lineaJuego.add(ficha);
+                    System.out.println("Ingrese el lado2 de su ficha: ");
+                    int lado2 = sc.nextInt();
+                    comodin.setLado2(lado2);
+                    jugador.removerFicha(ficha);
+                }
+                
+                r = true;
+            }
+        }
+        return r;
+    }
     
 }
