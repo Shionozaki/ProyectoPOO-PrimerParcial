2c74b2af27a618c067d52c957658e037027af6be
Shionozaki <152203621+Shionozaki@users.noreply.github.com>
2023-12-02 16:25:19 -0500

    Commit hecho por Alain Martínez Vega

--- src/main/java/ec/edu/espol/juego/Jugador.java
+++ src/main/java/ec/edu/espol/juego/Jugador.java
@@ -3,11 +3,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ec.edu.espol.juego;
+import java.util.ArrayList;
 
 /**
  *
- * @author user
+ * @author Alain Martínez Vega
  */
 public class Jugador {
+    private String nombre;
+    private ArrayList<Ficha> mano;
+
+    public Jugador(String nombre, ArrayList<Ficha> mano)
+    {
+        this.nombre = nombre;
+        this.mano = mano;
+        
+    }
+
+    public ArrayList<Ficha> getMano() {
+        return mano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
     
+    public Ficha getFicha(int i)
+    {
+        if(mano.get(i)!=null)
+        {
+            return mano.get(i);
+        }
+        else
+            return null;
+        
+    }
+    
+    public String imprimirMano()
+    {
+        StringBuilder m = new StringBuilder();
+        int cont =0;
+        for(Ficha ficha: mano)
+        {
+            if(cont<(mano.size()-1))
+            {
+                m.append(ficha.toString()+"-");
+            }
+            else
+            {
+                m.append(ficha.toString());
+            }
+            cont++;
+        }
+        
+        return m.toString();
+    }
+    
+    public void removerFicha(Ficha ficha)
+    {
+       mano.remove(ficha);
+       
+    }
 }
