5099e71ef1ce3d385fadd7cecb2529334d976bcc
Shionozaki <alainmartinez1203@gmail.com>
2023-12-02 16:16:10 -0500

    Primer commit

--- src/main/java/ec/edu/espol/juego/JuegoDomino.java
+++ src/main/java/ec/edu/espol/juego/JuegoDomino.java
@@ -1,13 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package ec.edu.espol.juego;
 
 /**
  *
  * @author user
  */
-public class JuegoPro {
-    
+public class JuegoDomino {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
