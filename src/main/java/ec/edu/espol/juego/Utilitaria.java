0c87618dcc4b5aad4e60e838b3420e70e1236bce
Shionozaki <152203621+Shionozaki@users.noreply.github.com>
2023-12-02 16:27:49 -0500

    Commit hecho por Alain Martínez Vega

--- src/main/java/ec/edu/espol/juego/Utilitaria.java
+++ src/main/java/ec/edu/espol/juego/Utilitaria.java
@@ -3,11 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ec.edu.espol.juego;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
- * @author user
+ * @author Alain Martínez Vega
  */
 public class Utilitaria {
     
+   public static ArrayList<Ficha> crearManoJugador()
+    {
+        Random dado = new Random();
+        
+        ArrayList<Ficha> fichas = new ArrayList<>();
+        
+        
+        for(int i = 0; i<5;i++)
+        {
+            int lado1 = (dado.nextInt(6-1+1)+1);
+            int lado2 = (dado.nextInt(6-1+1)+1);
+            
+            if( i==4)
+            {
+                FichaComodin comodin = new FichaComodin();
+                fichas.add(comodin);
+            }
+            
+            Ficha ficha = new Ficha(lado1,lado2);
+            fichas.add(ficha);
+        }
+        
+      return fichas;
+
+    } 
 }
